99d1e6cd9ca1c44e0429e69ff86f7d49b68952d2
akshadamore453 <akshadamore114@gmail.com>
2026-08-01 10:35:05 +0530

    Configure GitHub Pages deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,7 @@ function Landing() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/category/:slug" element={<CategoryPage />} />
